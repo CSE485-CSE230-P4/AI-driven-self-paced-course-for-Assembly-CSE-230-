41efbdf6ac5cd3a7f96c1c9170bd4ae907637fd2
kaus0311 <knegi2@asu.edu>
2025-11-13 13:39:34 -0700

    added all dependencies to req.txt

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,9 @@
+#checking connection
+import os
+from sqlalchemy import create_engine
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
 
 from fastapi import FastAPI
 from app.models import domain_models
